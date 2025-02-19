5e004fb11a3da03b88fb3b0bef4263c946fac827
Hieu <danhhieu123ns@gmail.com>
2025-02-19 21:12:01 +0700

    refactor searchByApi utility to enhance search functionality and UI components

--- src/utils/searchByApi.jsx
+++ src/utils/searchByApi.jsx
@@ -1,43 +1,151 @@
+import { Input, Space, Button, DatePicker } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import { Button, Input } from 'antd';
+import Highlighter from 'react-highlight-words';
 
-export const getColumnSearchProps = (placeholder, dataIndex) => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-        <div style={{ padding: 8 }}>
-            <Input
-                placeholder={`Tìm ${placeholder}`}
-                value={selectedKeys[0]}
-                onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-                onPressEnter={() => confirm()}
-                style={{ width: 188, marginBottom: 8, display: 'block' }}
-            />
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Button
-                    type="primary"
-                    onClick={() => confirm()}
-                    size="small"
-                    style={{ width: 90 }}
-                >
-                    Tìm
-                </Button>
-                <Button
-                    onClick={() => {
-                        clearFilters();
-                        confirm();
+import { useEffect, useRef, useState } from 'react';
+import dayjs from 'dayjs';
+
+const { RangePicker } = DatePicker;
+
+// Không khác gì lắm so với SearchInColumn.
+// QUAN TRỌNG: TỰ NÓ KO SEARCH ĐC. Cần phải sử dụng với onChange ở Table để search cùng filter bình thường
+export function getColumnSearchProps(title, dataIndex) {
+    const [searchText, setSearchText] = useState(''); // Update Highlight state
+    const [searchedColumn, setSearchedColumn] = useState(''); // Isolate Highlight to only searched column?
+    const searchInput = useRef(null); // Hold search input
+
+    const handleSearch = async (selectedKeys, confirm, dataIndex) => {
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+        confirm();
+    };
+
+    const handleReset = (setSelectedKeys, clearFilters, confirm) => {
+        setSearchText('');
+        setSelectedKeys([]);
+        clearFilters();
+        // reset();
+        confirm();
+    };
+
+    return {
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div className="p-2" onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Tìm kiếm trong ${title}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])} // Change search input into array to match selectedKeys form
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
                     }}
-                    size="small"
-                    style={{ width: 90 }}
-                >
-                    Xóa
-                </Button>
+                />
+                <Space className="flex justify-between">
+                    <Button
+                        type="text"
+                        onClick={() => {
+                            handleReset(setSelectedKeys, clearFilters, confirm);
+                        }}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                </Space>
             </div>
-        </div>
-    ),
-    filterIcon: filtered => (
-        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-    ),
-    onFilter: (value, record) =>
-        record[dataIndex]
-            ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
-            : '',
-});
+        ),
+        filterIcon: (filtered) => <SearchOutlined className={filtered ? 'stroke-primary' : ''} />,
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    };
+}
+
+export function getColumnSearchDateProps(title, dataIndex) {
+    const handleSearch = async (selectedKeys, confirm, dataIndex) => {
+        confirm();
+    };
+
+    const handleReset = (setSelectedKeys, clearFilters, confirm) => {
+        setSelectedKeys([]);
+        clearFilters();
+        // reset();
+        confirm();
+    };
+
+    return {
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div className="flex flex-col p-2" onKeyDown={(e) => e.stopPropagation()}>
+                <RangePicker
+                    format="DD/MM/YYYY"
+                    onChange={(date, dateString) => {
+                        setSelectedKeys(
+                            dateString ? dateString.map((entry) => dayjs(entry, 'DD/MM/YYYY').format('YYYY-MM-DD')) : []
+                        );
+                    }} // Change search input into array to match selectedKeys form
+                    size="small"
+                    style={{
+                        marginBottom: 8,
+                    }}
+                />
+                <Space className="flex justify-between">
+                    <Button
+                        type="text"
+                        onClick={() => {
+                            handleReset(setSelectedKeys, clearFilters, confirm);
+                        }}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => <SearchOutlined className={filtered ? 'stroke-primary' : ''} />,
+    };
+}
