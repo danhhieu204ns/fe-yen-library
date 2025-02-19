061ba70aff4045672da16205911f3399eeb362fd
Hieu <danhhieu123ns@gmail.com>
2025-02-19 20:59:41 +0700

    update ManageGenre import path and comment out unused menu items in itemsHeader

--- src/layouts/itemsHeader.jsx
+++ src/layouts/itemsHeader.jsx
@@ -9,38 +9,38 @@ const MenuItems = () => {
 
     // Định nghĩa các mục menu
     const menuItems = [
-        {
-            key: 'search',
-            label: (
-                <Link to={'/search'} className="flex items-center space-x-2">
-                    Tra cứu sách
-                </Link>
-            ),
-        },
-        {
-            key: 'volunteer',
-            label: (
-                <Link to={'/volunteer'} className="flex items-center space-x-2">
-                    Thiện nguyện
-                </Link>
-            ),
-        },
-        {
-            key: 'schedule',
-            label: (
-                <Link to={'/schedule'} className="flex items-center space-x-2">
-                    Lịch mở cửa
-                </Link>
-            ),
-        },
-        {
-            key: 'event',
-            label: (
-                <Link to={'/event'} className="flex items-center space-x-2">
-                    Sự kiện
-                </Link>
-            ),
-        },
+        // {
+        //     key: 'search',
+        //     label: (
+        //         <Link to={'/search'} className="flex items-center space-x-2">
+        //             Tra cứu sách
+        //         </Link>
+        //     ),
+        // },
+        // {
+        //     key: 'volunteer',
+        //     label: (
+        //         <Link to={'/volunteer'} className="flex items-center space-x-2">
+        //             Thiện nguyện
+        //         </Link>
+        //     ),
+        // },
+        // {
+        //     key: 'schedule',
+        //     label: (
+        //         <Link to={'/schedule'} className="flex items-center space-x-2">
+        //             Lịch mở cửa
+        //         </Link>
+        //     ),
+        // },
+        // {
+        //     key: 'event',
+        //     label: (
+        //         <Link to={'/event'} className="flex items-center space-x-2">
+        //             Sự kiện
+        //         </Link>
+        //     ),
+        // },
 
         // Chỉ thêm list sau nếu vai trò là 'admin'
         ...((role && role.includes('admin')) ? [
@@ -86,60 +86,60 @@ const MenuItems = () => {
                 },
             ],
         },
-        {
-            key: 'analize',
-            label: (
-                <div className="flex items-center space-x-2">
-                    <span>Thống kê số liệu</span>
-                    <DownOutlined />
-                </div>
-            ),
-            children: [
-                {
-                    key: '1',
-                    label: <Link to="/k/1">Thống kê</Link>,
-                },
-                {
-                    key: '2',
-                    label: <Link to="/k/2">Thống kê</Link>,
-                },
-                {
-                    key: '3',
-                    label: <Link to="/k/3">Thống kê</Link>,
-                },
-                {
-                    key: '4',
-                    label: <Link to="/k/4">Thống kê</Link>,
-                },
-                {
-                    key: '6',
-                    label: <Link to="/k/5">Thống kê</Link>,
-                },
-            ],
-        },
-        {
-            key: 'manage_user',
-            label: (
-                <Link to={'/manage/user'} className="flex items-center space-x-2">
-                    Quản lý người dùng
-                </Link>
-            ),
-        },
+        // {
+        //     key: 'analize',
+        //     label: (
+        //         <div className="flex items-center space-x-2">
+        //             <span>Thống kê số liệu</span>
+        //             <DownOutlined />
+        //         </div>
+        //     ),
+        //     children: [
+        //         {
+        //             key: '1',
+        //             label: <Link to="/k/1">Thống kê</Link>,
+        //         },
+        //         {
+        //             key: '2',
+        //             label: <Link to="/k/2">Thống kê</Link>,
+        //         },
+        //         {
+        //             key: '3',
+        //             label: <Link to="/k/3">Thống kê</Link>,
+        //         },
+        //         {
+        //             key: '4',
+        //             label: <Link to="/k/4">Thống kê</Link>,
+        //         },
+        //         {
+        //             key: '6',
+        //             label: <Link to="/k/5">Thống kê</Link>,
+        //         },
+        //     ],
+        // },
+        // {
+        //     key: 'manage_user',
+        //     label: (
+        //         <Link to={'/manage/user'} className="flex items-center space-x-2">
+        //             Quản lý người dùng
+        //         </Link>
+        //     ),
+        // },
 
         ] : []), 
         
-        // Cho user
-        ...((role && role.includes('user')) ? [
-            {
-                key: 'my_book_cart',
-                label: (
-                    <Link to={'/mybookcart'} className="flex items-center space-x-2">
-                        Giỏ sách của tôi
-                    </Link>
-                ),
-            },
+        // // Cho user
+        // ...((role && role.includes('user')) ? [
+        //     {
+        //         key: 'my_book_cart',
+        //         label: (
+        //             <Link to={'/mybookcart'} className="flex items-center space-x-2">
+        //                 Giỏ sách của tôi
+        //             </Link>
+        //         ),
+        //     },
     
-        ] : []), // Nếu không phải user, trả về mảng rỗng
+        // ] : []), // Nếu không phải user, trả về mảng rỗng
     ];
 
     return menuItems;
