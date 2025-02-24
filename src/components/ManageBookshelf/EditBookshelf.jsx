cd32bbfd2085867bd0bc75d370d4945f0667d8b0
Hieu <danhhieu123ns@gmail.com>
2025-02-24 08:34:01 +0700

    add bookshelf management components: CreateBookshelf, EditBookshelf, ImportBookshelf, and ShowInfoBookshelf

---
+++ src/components/ManageBookshelf/EditBookshelf.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect, memo } from 'react';
+import { Input, Typography, Col, Row, Modal, Select } from 'antd';
+import useBookshelfApi from 'src/services/bookshelfService';
+import { toast } from 'react-toastify';
+import ErrorMessage from 'src/utils/error/errorMessage';
+
+function EditBookshelf({ openModal, closeModal, handleReload, data }) {
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [errorMessages, setErrorMessages] = useState('');
+
+    const { updateBookshelf } = useBookshelfApi();
+
+    useEffect(() => {
+        if (openModal) {
+            if (data) {
+                setName(data?.name || '');
+                setStatus(data?.status || '');
+            }
+        }
+    }, [openModal, data]);
+
+    const handleEditBookshelf = async () => {
+        if (!name || name.trim().length === 0) {
+            setErrorMessages('Vui lòng nhập tên kệ sách');
+            return;
+        }
+
+        const updatedData = {
+            name: name.trim(),
+            status: status.trim(),
+        };
+        
+        try {
+            const result = await updateBookshelf(data.id, updatedData);
+
+            if (result?.message) {
+                toast.success('Cập nhật kệ sách thành công');
+                resetForm();
+                closeModal();
+                handleReload();
+            } else {
+                toast.error('Cập nhật kệ sách thất bại. Vui lòng thử lại!');
+            }
+        } catch (error) {
+            console.error('Update error:', error);
+            toast.error('Cập nhật kệ sách thất bại. Vui lòng thử lại!');
+        }
+    };
+
+    const resetForm = () => {
+        setName('');
+        setStatus('');
+        setErrorMessages('');
+    };
+
+    return (
+        <Modal
+            title={<div className="text-lg">Sửa thông tin kệ sách</div>}
+            open={openModal}
+            onCancel={() => {
+                resetForm();
+                closeModal();
+            }}
+            onOk={handleEditBookshelf}
+            maskClosable={false}
+            centered
+            width={600}
+            style={{ 
+                top: 20,
+                padding: '20px',
+                borderRadius: '6px',
+                background: '#fff',
+            }}
+        >
+            <div className="p-4">
+                <Row gutter={[0, 16]}>
+                    <Col span={24}>
+                        <Typography.Text strong className="text-base">Tên kệ sách:</Typography.Text>
+                        <Input
+                            placeholder="Nhập tên kệ sách"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                                setErrorMessages('');
+                            }}
+                            className="mt-2"
+                        />
+                    </Col>
+                    <Col span={24}>
+                        <Typography.Text strong className="text-base">Tình trạng:</Typography.Text>
+                        <Input
+                            placeholder="Nhập tình trạng kệ sách"
+                            value={status}
+                            onChange={(e) => setStatus(e.target.value)}
+                            className="mt-2"
+                        />
+                    </Col>
+                    <ErrorMessage message={errorMessages} />
+                </Row>
+            </div>
+        </Modal>
+    );
+}
+
+export default memo(EditBookshelf);
---
+++ src/services/bookshelfService.js
@@ -0,0 +1,139 @@
+import useHttpPrivate from "src/hooks/useHttpPrivate";
+
+export const useBookshelfApi = () => {
+
+    const httpPrivate = useHttpPrivate();
+
+    const getAllBookshelfs = async () => {
+        try {
+            const res = await httpPrivate.get('/bookshelf/all');
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    };
+
+    const getBookshelf = async (id) => {
+        try {
+            const res = await httpPrivate.get(`/bookshelf/${id}`);
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const getBookshelfByPage = async (page, pageSize) => {
+        try {
+            const res = await httpPrivate.get(`/bookshelf/pageable?page=${page}&page_size=${pageSize}`);
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const createBookshelf = async (bookshelf) => {
+        try {
+            const res = await httpPrivate.post('/bookshelf/create', {
+                ...bookshelf,
+            });
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const updateBookshelf = async (id, bookshelf) => {
+        try {
+            const res = await httpPrivate.put(`/bookshelf/update/${id}`, {
+                ...bookshelf,
+            });
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const deleteBookshelf = async (id) => {
+        try {
+            const res = await httpPrivate.delete(`/bookshelf/delete/${id}`);
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const deleteListBookshelf = async (ids) => {
+        try {
+            const res = await httpPrivate.delete(`/bookshelf/delete-many`, {data: {ids: ids}});
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const searchBookshelf = async (search, page, page_size) => {
+        try {
+            const res = await httpPrivate.get(`/bookshelf/search?page=${page}&page_size=${page_size}`, {
+                ...search,
+            });
+            return res.data;
+        } catch (error) {
+            console.error('Get roles error:', error);
+            return [];
+        }
+    }
+
+    const importBookshelf = async (formData) => {
+        try {
+            const response = await httpPrivate.post('/bookshelf/import', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            return response
+        } catch (error) {
+            return error.response;
+        }
+    };
+
+    const exportBookshelf = async () => {
+        try {
+            const res = await httpPrivate.get('/bookshelf/export', {
+                responseType: 'blob',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                }
+            });
+            return res;
+        } catch (error) {
+            console.error('Export error:', error);
+            return null;
+        }
+    };
+
+    let bookshelfService = {
+        getAllBookshelfs, 
+        getBookshelf, 
+        getBookshelfByPage,
+        createBookshelf,
+        updateBookshelf,
+        deleteBookshelf,
+        deleteListBookshelf,
+        searchBookshelf,
+        importBookshelf,
+        exportBookshelf,
+    };
+
+    return bookshelfService; 
+}
+
+export default useBookshelfApi;
+
