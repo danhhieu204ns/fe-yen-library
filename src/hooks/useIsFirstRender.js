f17af82efdaa0d2c3d698fa47c482d0f7240699f
Hieu <danhhieu123ns@gmail.com>
2025-02-28 13:48:58 +0700

    refactor: remove unused hooks and admin service for cleaner codebase

--- src/hooks/useIsFirstRender.js
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect } from 'react';
-
-export const useIsFirstRender = () => {
-  const isFirstRender = useRef(true);
-  useEffect(() => {
-    isFirstRender.current = false;
-  }, []);
-  return isFirstRender.current;
-};
